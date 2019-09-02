c3814d8b724e76ac590ea5ae5e4a29d88fae8ee3
RikuHirose <rikuparkour9@gmail.com>
2019-09-02 23:54:50 +0900

    get applications

---
+++ helper.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * 
+ */
+class helper
+{
+
+  function __construct()
+  {
+    # code...
+  }
+
+  /**
+  * csvに書き込んだdataをarrayで取り出す
+  **/
+  public function csvToArray($filePath)
+  {
+    $file = array();
+    foreach(glob($filePath) as $f){
+      if(is_file($f)){
+        $f = htmlspecialchars($f);
+        $file[] = $f;
+      }
+    }
+
+
+    $csv  = array();
+    $fp   = fopen($file[0], "r");
+
+    while (($data = fgetcsv($fp, 0, ",")) !== FALSE) {
+      $csv[] = $data;
+    }
+    fclose($fp);
+
+    return $csv;
+  }
+
+  /**
+  * dataをcsvに書きこむ
+  **/
+  public function writeToCsv($filePath, $data)
+  {
+    $f = fopen($filePath, "a");
+    // 正常にファイルを開くことができていれば、書き込みます。
+    if ( $f ) {
+
+      fputcsv($f, $data);
+    }
+    // ファイルを閉じます。
+    fclose($f);
+  }
+}
